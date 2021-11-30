e025f5c4416eca0e0ef0bf45c2d532fddfb91d03
acastronuovo5 <55922639+acastronuovo5@users.noreply.github.com>
2021-11-29 19:06:44 -0500

    Necessary files to start something
    python client example, python server example, to do list, attempt at implant set up

---
+++ implant.cpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+
+
+//DLL stealer
+
+
+
+
+//EXPORT REFLECTIVE LOADER FUNCTION
+//possibly import library that loads for me
+
+
+//DLL MAIN
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+
+
+
+
+
+
+    return TRUE;
+}
