daa48dc5fd259314f310d98aeb9c9b13af4c5b8f
acastronuovo5 <55922639+acastronuovo5@users.noreply.github.com>
2021-12-01 10:21:28 -0500

    Reflective dll organization
    Organizing my thoughts on reflective dll

--- implant.cpp
+++ implant.cpp
@@ -1,19 +1,42 @@
 #include <windows.h>
-
+#include <stdio.h>
+#include <iostream>
 
 //DLL stealer
 
 
+//gather info (Situational Awareness)
+
+
+//Ability to follow commmands from C2
+
+//Characteristics pre configured??
+
 
 
 //EXPORT REFLECTIVE LOADER FUNCTION
-//possibly import library that loads for me
+//possibly import library that loads for me?
 
+
+// You can use this value as a pseudo hinstDLL value (defined and set via ReflectiveLoader.c)
+extern HINSTANCE hAppInstance;
+
+HMODULE DllHandle = NULL; 
 
 //DLL MAIN
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
 
-
+    switch( fdwReason ) 
+    { 
+		case DLL_PROCESS_ATTACH:
+			hAppInstance = hinstDLL;
+			MessageBoxA( NULL, "Hello from DllMain!", "Reflective Dll Injection", MB_OK );
+			break;
+		case DLL_PROCESS_DETACH:
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+            break;
+    }
 
 
 
